15355518815d0e6962e56988a58f487dce3d25c9
Alexander Rudoi <rudoi.alexandru25@gmail.com>
2026-05-03 17:56:42 +0300

    feat: wire DataProviders into app root

--- Salon.Web/src/main.jsx
+++ Salon.Web/src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { ThemeProvider } from './context/ThemeContext';
+import DataProviders from './context/DataProviders';
 import App from './App.jsx';
 import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <DataProviders>
+        <App />
+      </DataProviders>
     </ThemeProvider>
   </StrictMode>,
 );
